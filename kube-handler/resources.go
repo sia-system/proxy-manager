5452eb19cfe4425b6def285da3eeb768a41db4d7
Dmitri Astafiev <demius.md@gmail.com>
2021-03-17 13:34:20 +0200

    may be aggregate config source

--- kube-handler/resources.go
+++ kube-handler/resources.go
@@ -66,6 +66,10 @@ func MakeEndpoint(clusterName, ns string, endpoints []EndpointInfo) *endpoint.Cl
 // MakeCluster creates a cluster using either ADS or EDS.
 func MakeCluster(clusterName, ns string, alpn bool) *cluster.Cluster {
 	edsSource := &core.ConfigSource{
+		ConfigSourceSpecifier: &core.ConfigSource_Ads{
+			Ads: &core.AggregatedConfigSource{},
+		},
+		/*
 		ConfigSourceSpecifier: &core.ConfigSource_ApiConfigSource{
 			ApiConfigSource: &core.ApiConfigSource{
 				ApiType: core.ApiConfigSource_GRPC,
@@ -76,6 +80,7 @@ func MakeCluster(clusterName, ns string, alpn bool) *cluster.Cluster {
 				}},
 			},
 		},
+		*/
 		ResourceApiVersion: core.ApiVersion_V3,
 	}
 
